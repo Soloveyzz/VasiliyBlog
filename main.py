0f37b7b16b6e5fce90504746a6a85331ae6eaad2
Solovey <soloveyzz47@gmail.com>
2022-09-05 22:52:57 +0300

    SECRET_KEY and delete file

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ def admin_only(function):
 
 
 app = Flask(__name__)
-SECRET_KEY = os.environ['SECRET_KEY']
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
